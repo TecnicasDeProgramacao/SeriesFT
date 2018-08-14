0070485878bb8b5ee9fa1a2e042f98031255165d
KIN MAX PIAMOLINI GUSMAO <16104046@pucrs.br>
2018-08-14 10:49:26 -0300

    Acresencta classe Trig

--- src/main/java/com/bcopstein/FuncoesST/Trig.java
+++ src/main/java/com/bcopstein/FuncoesST/Trig.java
@@ -4,19 +4,48 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Trig {
-	public Iterator<Double> getTermos(){
-		// TODO:
-		throw new UnsupportedOperationException();
+public class Trig 
+{
+	
+	private List<Double> lstValores;
+	
+	public Trig()
+	{
+		this.lstValores = new ArrayList<Double>();
+	}
+	
+	public Iterator<Double> getTermos()
+	{
+		return this.lstValores.iterator();
 	}
 
-	public double cos(double x) {
-		// TODO:
-		throw new UnsupportedOperationException();
+	public double cos(double x) 
+	{
+		double termo = 1.0;
+		int n = 0;
+		while(termo >= 10E-6)
+		{
+			termo = Math.pow(-1, n);
+			termo = termo/Util.fatorial(2*n);
+			termo = termo * Math.pow(x, 2*n);
+			this.lstValores.add(termo);
+			n++;
+		}
+		return Util.somatorio(0, n, lstValores);
 	}
 
-	public double sin(double x) {
-		// TODO:
-		throw new UnsupportedOperationException();
+	public double sin(double x) 
+	{
+		double termo = 1.0;
+		int n = 0;
+		while(termo >= 10E-6)
+		{
+			termo = Math.pow(-1, n);
+			termo = termo/Util.fatorial((2*n) + 1);
+			termo = termo * Math.pow(x, ((2*n) + 1));
+			this.lstValores.add(termo);
+			n++;
+		}
+		return Util.somatorio(0, n, lstValores);
 	}
 }
